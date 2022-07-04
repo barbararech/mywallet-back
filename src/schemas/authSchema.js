9dc73a679567cd0793a0b3083a635031c0f4b5f9
barbararech <rech.brb@gmail.com>
2022-07-04 09:29:50 -0300

    chore: adjust code for deploy on heroku

--- src/schemas/authSchema.js
+++ src/schemas/authSchema.js
@@ -3,7 +3,8 @@ import joi from "joi";
 export const authSignUpSchema = joi.object({
   name: joi.string().required(),
   email: joi.string().email().required(),
-  password: joi.string().required(),
+  password: joi.string().min(5).required(),
+  confirmPassword: joi.any().valid(joi.ref("password")).required(),
 });
 
 export const authLoginSchema = joi.object({
